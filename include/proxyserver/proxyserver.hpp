85531e8ca87f4b612f5dd2e4a875fabd93332098
jack blue <jack.mfv@gmail.com>
2016-09-30 10:13:43 +0800

    添加了服务端作为client向远程服务器发起请求部分

--- include/proxyserver/proxyserver.hpp
+++ include/proxyserver/proxyserver.hpp
@@ -12,9 +12,12 @@
 #include <boost/shared_ptr.hpp>
 #include <boost/enable_shared_from_this.hpp>
 #include <boost/asio.hpp>
+#include <iostream>
+#include <vector>
 
 namespace my {
 
+/* 处理客户端连接以后的socket，比如写入相应的信息*/
 class ProxyServerConnection : public boost::enable_shared_from_this<ProxyServerConnection> {
  public:
   typedef boost::shared_ptr<ProxyServerConnection> proxser_pointer;
@@ -36,12 +39,13 @@ class ProxyServerConnection : public boost::enable_shared_from_this<ProxyServerC
 
   //异步写socket时使用的句柄函数
   void WriteConnectionSocketHandle(const boost::system::error_code& /*error*/,
-                             std::size_t /*bytes_transferred*/) {
+                                   std::size_t /*bytes_transferred*/) {
   }
   boost::asio::ip::tcp::socket socket_;
   std::string sendmessage_;
 };
 
+/*绑定地址及端口，监听client的socket连接，发送消息给连接的socket*/
 class ProxyServer {
  public:
   ProxyServer(boost::asio::io_service& io_service, boost::asio::ip::tcp::endpoint& endpoint)
@@ -54,14 +58,50 @@ class ProxyServer {
   void ProcessRequestOfClients();
 
   //包装ProxyServerConnection对象，并调用其中的写socket函数
-  void WriteHandleOfClientRequests(ProxyServerConnection::proxser_pointer new_connection,
-                         const boost::system::error_code& error) {
+  void WriteRequestsOfClientHandle(ProxyServerConnection::proxser_pointer new_connection,
+                                   const boost::system::error_code& error) {
     if (!error) {
       new_connection->WriteConnectionSocket();
     }
     ProcessRequestOfClients();
   }
   boost::asio::ip::tcp::acceptor acceptor_;
+};
+
+class ClientOfLaunchRequest {
+ public:
+  ClientOfLaunchRequest(boost::asio::io_service io_service, boost::asio::ip::tcp::socket socket,
+                        std::string urlOfLanuchRequest)
+      : io_service_(io_service),
+        socket_(socket),
+        urlOfLanuchRequest_(urlOfLanuchRequest) {
+    std::cout << "ClientOfLaunchRequest begin work." << std::endl;
+    ResolveUrl(urlOfLanuchRequest_);
+  }
+  /*连接远程服务器及端口*/
+  void ConnectRemoteServer(char* remoteServerHostname, int remoteServerPort);
+  void SendClientOfRequest(std::string& urlOfRequest);
+  void ContentOfReponse();
+
+ private:
+  ClientOfLaunchRequest() {                       //不允许构造无参数构造函数
+  }
+  boost::asio::io_service io_service_;
+  boost::asio::ip::tcp::socket socket_;
+  std::string urlOfLanuchRequest_;
+
+  /*远程服务器地址，端口，已经请求内容*/
+  char* remoteServerHostname_ = "127.0.0.1";
+  int remoteServerPort_ = 80;
+  std::string pathOfLauchRequest_;
+
+  /*存储从服务器返回内容*/
+  const int kMaxBytesOfBuff = 2048;
+  std::vector<char> contentOfReponse_(kMaxBytesOfBuff);
+
+  /* 解析url,并且写入成员变量remoteServerHostname_及remoteServerPort_，pathOfLauchRequest_*/
+  void ResolveUrl(const std::string& urlOfLanuchRequest);
+
 };
 
 }  //namespace my
--- src/proxyserver/proxyserver.cc
+++ src/proxyserver/proxyserver.cc
@@ -6,6 +6,7 @@
  */
 
 #include "proxyserver/proxyserver.hpp"
+#include "boost/lexical_cast.hpp"
 
 //写socket用测试函数
 std::string hello() {
@@ -28,7 +29,74 @@ void my::ProxyServer::ProcessRequestOfClients() {
       acceptor_.get_io_service());
   acceptor_.async_accept(
       newConnection->GetSocket(),
-      boost::bind(&ProxyServer::WriteHandleOfClientRequests, this, newConnection,
+      boost::bind(&ProxyServer::WriteRequestsOfClientHandle, this, newConnection,
                   boost::asio::placeholders::error));
+}
+
+/*
+ void ConnectRemoteServer(const char* remoteServerHostname, int remoteServerPort);
+ void SendClientOfRequest(std::string& contentOfRequest);
+ void ContentOfReponse();
+ */
+
+/*连接远程服务器地址和端口，绑定socket_*/
+void my::ClientOfLaunchRequest::ConnectRemoteServer(char* remoteServerHostname,
+                                                    int remoteServerPort) {
+  remoteServerHostname = remoteServerHostname_;
+  remoteServerPort = remoteServerPort_;
+  boost::asio::ip::tcp::resolver resolver(socket_.get_io_service());
+  boost::asio::ip::tcp::resolver::query queryEndpoints(
+      remoteServerHostname, boost::lexical_cast<std::string>(remoteServerPort));
+  boost::asio::ip::tcp::resolver::iterator endpoint_iterator = resolver.resolve(queryEndpoints);
+  boost::asio::ip::tcp::resolver::iterator end;
+  boost::system::error_code ec = boost::asio::error::host_not_found;
+
+  for (; ec && endpoint_iterator != end; ++endpoint_iterator) {
+    socket_.close();
+    socket_.connect(*endpoint_iterator, ec);
+  }
+  if (ec) {
+    std::cout << "can't connect." << std::endl;
+    throw boost::system::system_error(ec);
+  }
+  std::cout << "connect success." << std::endl;
+}
+
+/*构造请求*/
+void my::ClientOfLaunchRequest::ResolveUrl(const std::string& urlOfLanuchRequest) {
+    std::string url = urlOfLanuchRequest;
+    std::size_t first_match_pos = url.find_first_of("/");                          //查询第一个"/"出现的pos值
+    std::string urlProtocol = url.substr(0, (first_match_pos - 1));          //得到url请求的头，http或者https
+
+    /*分解出url中的host，port，内容*/
+    const std::string requestHttp("http");
+    const std::string requestHttps("https");
+    if (urlProtocol.compare(requestHttp) == 0 || urlProtocol.compare(requestHttps) == 0) {
+      std::size_t second_match_pos = url.find_first_of("/", first_match_pos + 1);  //查询第二个"/"出现的pos值
+      std::size_t third_match_pos = url.find_first_of("/", second_match_pos + 1);  //查询第三个"/"出现的pos值
+
+      /*   */
+      remoteServerHostname_ = url.substr(second_match_pos + 1,
+                                         third_match_pos - second_match_pos - 1);
+
+      /* */
+      pathOfLauchRequest_ = url.substr(third_match_pos);
+
+
+    }
+
+
+}
+
+void my::ClientOfLaunchRequest::SendClientOfRequest(std::string& urlOfRequest) {
+  std::string requestMethod = "GET";
+  std::string urlOfLanuchRequest_;
+  std::string httpVersion = "HTTP/1.0\r\n";
+  std::string hostname = "";
+
+}
+
+void my::ClientOfLaunchRequest::ContentOfReponse() {
+
 }
 
